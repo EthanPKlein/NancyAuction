af2f61d242a11c6918f478e87b63522b4baded1d
EthanPKlein <eklein@icct.com>
2016-05-16 22:27:12 -0400

    Adding in some (currently failing) unit tests

---
+++ src/NancyAuction/NancyAuctionTests/NancyTests.cs
@@ -0,0 +1,32 @@
+﻿
+using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+
+
+namespace NancyAuctionTests
+{
+
+    public class NancyTests
+    {
+
+        [Test]
+        public void Should_return_status_ok_when_route_exists()
+        {
+            // Given
+            //var bootstrapper = new NancyAuction.Bootstrapper();
+            //var browser = new Browser(bootstrapper);
+
+            var browser = new Browser(with => with.Module(new NancyAuction.Modules.AuctionModule()));
+
+            // When
+            var result = browser.Get("/simpleAuction/", with => {
+                with.HttpRequest();
+            });
+
+            // Then
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+    }
+}
