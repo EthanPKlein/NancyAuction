ef31d0bce0c4be8aa44d2231512add271d1405ac
EthanPKlein <eklein@icct.com>
2016-05-17 09:36:20 -0400

    Added second unit test

--- src/NancyAuction/NancyAuctionTests/NancyTests.cs
+++ src/NancyAuction/NancyAuctionTests/NancyTests.cs
@@ -13,18 +13,33 @@ namespace NancyAuctionTests
         [Test]
         public void Should_return_status_ok_when_route_exists()
         {
-            // Given
-            //var bootstrapper = new NancyAuction.Bootstrapper();
-            //var browser = new Browser(bootstrapper);
 
             var browser = new Browser(with => with.Module(new NancyAuction.Modules.AuctionModule()));
 
-            // When
-            var result = browser.Get("/simpleAuction/", with => {
+            var result = browser.Get("simpleAuction/unitTestPass", with => {
                 with.HttpRequest();
             });
 
-            // Then
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Test]
+        public void Should_return_status_ok_when_view_exists()
+        {
+
+            var browser = new Browser(new NancyAuction.Bootstrapper());
+
+
+            var result = browser.Get("simpleAuction/");
+
+            var response = browser.Get("/login", (with) => { ​
+                with.HttpRequest(); ​
+                with.Query("error", "true"); ​
+                with.Body("body");
+                with.Certificate();
+            });
+
+            // failing because the views aren't being routed properly.  This should be handled in a bootstrapper.
             Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
         }
 
