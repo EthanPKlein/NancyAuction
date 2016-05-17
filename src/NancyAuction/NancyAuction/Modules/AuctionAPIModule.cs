1d89a7a8b98cad3fca31bfd5f989ccd8dafeae79
EthanPKlein <eklein@icct.com>
2016-05-16 20:48:06 -0400

    Adding in a separate page to demo off some API calls

---
+++ src/NancyAuction/NancyAuction/Modules/AuctionAPIModule.cs
@@ -0,0 +1,30 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+using NancyAuction.Data;
+using NancyAuction.Models;
+
+namespace NancyAuction.Modules
+{
+    public class AuctionAPIModule : NancyModule
+    {
+        public AuctionAPIModule() : base("/api")
+        {
+
+            Get["/template"] = _ =>
+            {
+                return View["template.html"];
+            };
+
+            Get["/entry/{id}"] = parameters =>
+            {
+                int id = parameters.id;
+                var output = AuctionList.GetAuctionEntry(id);
+
+                return Negotiate
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithContentType("applicaton/json")
+                    .WithModel(output);
+            };
+        }
+    }
+}
